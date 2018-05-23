4b3b0d9f1ad2df31a4b84614a4ea5b37c2d60dfa
RICHDADIFY <39533634+RICHDADIFY@users.noreply.github.com>
2018-05-23 09:55:24 +0100

    Add files via upload

---
+++ new.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+
+<link href="../style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="container">
+<header>
+	<div class="logo">HELLO LOGO</div>
+	<div class="clear"></div>
+	<div>COMPANYNAME</div>
+	<div class="navmenu">
+		<ul>
+			<li>HOME</li>
+			<li>ABOUT</li>
+			<li>CONTACT</li>
+			<li>OUR STORY</li>
+			<li>GALLERY</li>
+		</ul>
+	</div>
+	
+</header>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</div>
+	
+</body>
+</html>
